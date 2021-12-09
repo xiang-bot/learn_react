f9f7322148144f1ab78710de073b9accd21e4681
wudi <1062393554@qq.com>
2021-12-09 15:15:40 +0800

    组件里传递内容

---
+++ react-learn/src/Clop.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Clop(props) {
+    console.log(props)
+    return (
+        <div className='clop'>
+            <h1>111111111111</h1>
+            {props.children1}
+        </div>
+    )
+}
--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Clop from './Clop'
 
-ReactDOM.render(<h1>111</h1>,document.getElementById("root"))
+ReactDOM.render(<Clop
+    children1={<>hhhhhhhh</>}>
+</Clop>,document.getElementById("root"))
